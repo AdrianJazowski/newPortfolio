15ffde295b5eb601d420f6409bcc2825b0e0072d
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-03 17:54:26 +0200

    add section about me

---
+++ src/Components/ImageSection.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import React from "react";
+
+function ImageSection() {
+  return (
+    <section className="ImageSection">
+      <div className="about-info">
+        <h4>
+          <span>Cześć</span> miło mi że wpadłeś,
+        </h4>
+        <p className="about-text">
+          skoro trafiłeś na tą stronę, to zapewne wiesz że szukam pracy jako
+          Junior Fronted Developer. Pochodzę z okolic Krakowa, w wolnym czasie
+          lubię grać w gry planszowe i programować.
+          <tr />
+        </p>
+        <p className="about-text">
+          Obecnie poszukuje pracy, gdzie będę mógł rozpocząć swoją przygodę jako
+          Frontend Developer. Rozważam pracę zdalną, jak i relokacje. Zapraszam
+          Cię to przejrzenia mojej strony, gdzie możesz zobaczyć moje projekty
+          oraz dowiedzieć się co potrafię.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default ImageSection;
